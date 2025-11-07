e43f7d4be9593ed4a918fae9e2e9e16c33ebdf75
NSH517 <tmdgusqufsla@gmail.com>
2025-11-07 18:11:21 +0900

    15일 차

---
+++ JavaFX/데이터전달/src/application/CController.java
@@ -0,0 +1,45 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CController implements DataReceiver{
+	
+	private Object receiveData;
+
+    @FXML
+    private TextField input;
+    
+    @FXML
+    void initialize() {
+    	// 1번
+    	System.out.println("# initialize #");
+    	System.out.println("receiveData : " + this.receiveData);
+    	
+    	// 3번
+    	Platform.runLater(() -> {
+    		System.out.println("# runLater #");
+        	System.out.println("receiveData : " + this.receiveData);
+    		
+    	});
+    }
+    // 2번
+    @Override
+    public void receiveData(Object data) {
+    	if ( data instanceof String text) {
+    		input.setText(text);
+    	}
+    	this.receiveData = data;
+    	System.out.println("# receiveData() #");
+    	System.out.println("receiveData : " + receiveData);
+    }
+
+    @FXML
+    void to(ActionEvent event) {
+
+    }
+
+
+}
